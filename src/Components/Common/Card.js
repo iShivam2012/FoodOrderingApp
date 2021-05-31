fd32333a4b4051ae72bdb3e44c39c433b93c10c6
iShivam2012 <shivam.gupta97a@gmail.com>
2021-06-01 03:43:29 +0530

    adding Card and Input Component in common

---
+++ src/Components/Common/Card.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+import classes from "./Card.module.css";
+
+const Card = (props) => {
+  console.log("card comp");
+  return <div className={classes.card}>{props.children}</div>;
+};
+
+export default Card;
