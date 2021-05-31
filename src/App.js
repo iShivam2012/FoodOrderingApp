96fbad480fc0846fda6fb29ee1398699816913ed
iShivam2012 <shivam.gupta97a@gmail.com>
2021-06-01 03:43:58 +0530

    adding Meal Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <div>
       <Header />
+      <Meals />
     </div>
   );
 }
